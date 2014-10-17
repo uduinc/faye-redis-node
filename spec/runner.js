ebd7f3e079312dbb9ff1c8372b9cd8507b012d93
Dave Yeu <daveyeu@gmail.com>
2014-10-17 08:37:18 -0400

    Modify Faye logging level when running tests

--- spec/runner.js
+++ spec/runner.js
@@ -5,4 +5,6 @@ Faye = require('../vendor/faye/build/node/faye-node')
 require('../vendor/faye/spec/javascript/engine_spec')
 require('./faye_redis_spec')
 
+// Faye.Logging.logLevel = 'debug';
+
 JS.Test.autorun()
